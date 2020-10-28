7993d1b36714771a6e5148b9fb13a0e08292c11b
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-10-27 18:12:17 -0600

    Agrego el ciclo

---
+++ Codigo.py
@@ -0,0 +1,17 @@
+import sys 
+
+
+
+
+
+
+try:
+    opcion=1
+    while opcion==1:
+        separador=("*"*40)
+        print("-"*30 +"MENU PRINCIPAL" + "-"*30)
+        print("1-Sacar el PIB por el Metodo del Ingreso\n2-Sacar el PIB por el metodo del Gasto\n3-Sacar el Indice de Precios,Inflacion y Producto Interno Bruto Real")
+        print("4-Sacar la Tasa de Desempleo")
+        print("-"*30)
+        menu=int(input("Ingrese el numero de opcion que desea ejecutar : "))
+        print("-"*30)
